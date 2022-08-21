7d7bd225116b5b286fd19820e130ce68609bce87
Chris Hobbs <c.hobbs@skybornetech.com>
2022-08-21 20:08:55 +1000

    Basic back and forth functionality working

--- main.py
+++ main.py
@@ -3,7 +3,8 @@ import sys
 
 from PyQt5.QtWidgets import QApplication
 from PyQt5.QtWidgets import QWidget
-from PyQt5.QtWidgets import QMainWindow
+from PyQt5.QtWidgets import QMainWindow, QFrame
+from PyQt5.QtWidgets import QVBoxLayout, QLabel
 from PyQt5.QtGui import QPainter, QBrush, QPen
 from PyQt5.QtCore import Qt, QTimer, QRect
 
@@ -40,22 +41,19 @@ class Paddle(QWidget):
 class Ball(QWidget):
     radius = 20
 
-    vx = 0 # pixels / sec
-    vy = 0
+    vx = 300 # pixels / sec
+    vy = 30
 
     def __init__(self,x,y,delta_ms):
         super().__init__()
-        self.x = int(x/2)
-        self.y = int(y/2)
-        self.delta_s = delta_ms 
-        self.vx = y * self.delta_s
+        self.x = int(x / 2)
+        self.y = int(y / 2)
+        self.delta_ms = delta_ms
 
 
     def update_ball(self):
-        self.x = int(self.x + self.vx * self.delta_s)
-        print(self.x)
-
-        # self.x = int(self.x + self.vx * self.delta_ms)
+        self.x = int(self.x + self.vx * self.delta_ms / 1000)
+        self.y = int(self.y + self.vy * self.delta_ms / 1000)
 
     def draw_ball(self):
         return QRect(self.x,self.y,self.radius,self.radius)
@@ -73,18 +71,36 @@ class Window(QMainWindow):
         self.width = 680
         self.height = 500
 
-        self.p1 = Paddle(self.width,self.height)
-        self.p2 = Paddle(0,self.height)
-        self.ball = Ball(self.width,self.height,self.dt)
+        # self.layout = QVBoxLayout(self)
+        # self.layout.addWidget(QLabel('Cunt'))
+        # self.layout.addWidget(QFrame())
+        # self.setLayout(self.layout)
+
+
+        self.p1 = Paddle(self.width, self.height)
+        self.p2 = Paddle(0, self.height)
+        self.ball = Ball(self.width, self.height, self.dt)
  
         self.init_window()
 
         self.timer = QTimer(self)
-        self.timer.timeout.connect(self.ball.update_ball)
         self.timer.timeout.connect(self.timer_callback)
         self.timer.start(self.dt)
 
     def timer_callback(self):
+
+        is_contact1 =  self.p1.draw_paddle().intersected(self.ball.draw_ball())
+        is_contact2 =  self.p2.draw_paddle().intersected(self.ball.draw_ball())
+
+        if self.ball.y + self.ball.radius > self.height:
+            self.ball.vy = -self.ball.vy
+        elif self.ball.y - self.ball.radius < 0:
+            self.ball.vy = -self.ball.vy
+
+        if is_contact1 or is_contact2:
+            self.ball.vx = -self.ball.vx
+
+        self.ball.update_ball()
         self.update()
 
     def init_window(self):
@@ -114,6 +130,9 @@ class Window(QMainWindow):
         
         if event.key() == Qt.Key.Key_S:
             self.p2.move_down()
+
+        if event.key() == Qt.Key.Key_Q:
+            sys.exit()
 
         self.update()
 
